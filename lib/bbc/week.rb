eb73e14b8f23e85085fe1ac31f1dbcfefb669bdd
Nicholas Humfrey <njh@aelius.com>
2020-08-20 09:22:52 +0100

    Created skeleton Gem

---
+++ lib/bbc/week.rb
@@ -0,0 +1,8 @@
+require "bbc/week/version"
+
+module BBC
+  module Week
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/bbc/week/version.rb
@@ -0,0 +1,5 @@
+module BBC
+  module Week
+    VERSION = "0.1.0"
+  end
+end
